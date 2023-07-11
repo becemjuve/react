e6dab73c8ef1bc994694746eb8fc499ffabc32ba
becem <becem.1897@hotmail.com>
2023-07-11 16:49:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,150 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import {Navbar,Container} from 'react-bootstrap'
+
+
+function App() {
+  return (
+    <>
+      <div className='App'>
+
+      <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Checkpoint </Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+          <Navbar.Text>
+            Signed in as: <a href="#login">Becem Alibi</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+        
+ 
+        <form className='container'>
+          <div className='row'>
+            <div className='rounded-4 shadow m-auto mt-5 border col-4 bg-body'>
+              <div className="mb-3">
+                <label htmlFor="exampleInputEmail1" className="form-label mt-3">Email address</label>
+                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+              </div>
+              <div className="mb-3">
+                <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                <input type="password" className="form-control" id="exampleInputPassword1" />
+              </div>
+              <div className="mb-3 form-check">
+                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+              </div>
+              <div className="container d-flex justify-content-between">
+                <button type="submit" className="btn btn-primary mb-4">Log In</button>
+                <button type="submit" className="btn mb-4" onClick={(e) => {
+                  e.preventDefault();
+                  console.log("this is a function");
+                }}>Sign In</button>
+              </div>
+            </div>
+          </div>
+        </form>
+
+        
+        <footer className="text-center text-lg-start bg-light text-muted">
+          <section className="d-flex justify-content-center justify-content-lg-between p-4 border-bottom"></section>
+
+          <section className="shadow">
+            <div className="container text-center text-md-start mt-5 ">
+              <div className="row mt-3">
+                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">
+                    <i className="fas fa-gem me-3"></i>Company name
+                  </h6>
+                  <p>
+                    Here you can use rows and columns to organize your footer
+                    content. Lorem ipsum dolor sit amet, consectetur adipisicing
+                    elit.
+                  </p>
+                </div>
+
+                <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Products</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Angular
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      React
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Vue
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Laravel
+                    </a>
+                  </p>
+                </div>
+
+                <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Useful links</h6>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Pricing
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Settings
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Orders
+                    </a>
+                  </p>
+                  <p>
+                    <a href="#!" className="text-reset">
+                      Help
+                    </a>
+                  </p>
+                </div>
+
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Contact</h6>
+                  <p>
+                    <i className="fas fa-home me-3"></i> New York, NY 10012, US
+                  </p>
+                  <p>
+                    <i className="fas fa-envelope me-3"></i>
+                    info@example.com
+                  </p>
+                  <p>
+                    <i className="fas fa-phone me-3"></i> + 01 234 567 88
+                  </p>
+                  <p>
+                    <i className="fas fa-print me-3"></i> + 01 234 567 89
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+          <div className="text-center p-4">
+            © 2021 Copyright:
+            <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
+              Socialbook.com
+            </a>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
+}
+
+export default App;
